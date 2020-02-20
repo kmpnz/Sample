83611e85cf021075f0b33adffafcd6814ff50c94
Andreas Kompanez <andreas.kompanez@beekeeper.io>
2020-02-20 09:35:45 +0100

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Andreas Kompanez on 20.02.20.
+//  Copyright © 2020 Andreas Kompanez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
